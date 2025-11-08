8a67d9c3f5b865d9e6a3c0aeede1eb9541fd479c
Quitumba Ferreira <quitumba.ferreira@liferay.com>
2025-11-08 15:42:26 -0300

    DB.MIGRATION sql script create user table

---
+++ src/main/resources/db/migration/V01__CREATE_USER_TABLE.sql
@@ -0,0 +1,12 @@
+CREATE TABLE USER_TABLE
+(
+    user_id                  INTEGER(16)   	NOT NULL PRIMARY KEY,
+    email                	 VARCHAR(150) 	NOT NULL UNIQUE,
+    password                 VARCHAR(100) 	NOT NULL,
+    role               		 VARCHAR(20)  	NOT NULL,
+    status               	 tinyint        DEFAULT 1,
+    created_at               DATETIME     	NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_at              DATETIME       DEFAULT NULL,
+    last_login_at            DATETIME       DEFAULT NULL,
+    password_reset_token     VARCHAR(6)   	NULL
+);
